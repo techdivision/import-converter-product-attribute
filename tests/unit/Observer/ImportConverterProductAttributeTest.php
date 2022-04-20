60b59970072127ed3b670f0f68fd6e052eeebafb
Kenza Yamlahi <k.yamlahi@techdivision.com>
2022-04-20 15:23:01 +0200

    add dummy unit test

---
+++ tests/unit/Observer/ImportConverterProductAttributeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * PHP version 7
+ *
+ * @author    MET <met@techdivision.com>
+ * @copyright 2022 TechDivision GmbH <info@techdivision.com>
+ * @license   https://opensource.org/licenses/MIT
+ * @link      https://github.com/techdivision/import-ee
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Cache\Observer;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Dummy Test for Github workflow
+ *
+ * @author    MET <met@techdivision.com>
+ * @copyright 2022 TechDivision GmbH <info@techdivision.com>
+ * @license   https://opensource.org/licenses/MIT
+ * @link      https://github.com/techdivision/import-ee
+ * @link      http://www.techdivision.com
+ */
+class ImportConverterProductAttributeTest extends TestCase
+{
+
+    /**
+     * Dummy test implementation.
+     *
+     * @return void
+     */
+    public function testDummy()
+    {
+        $this->markTestSkipped('Still to implement');
+    }
+}
